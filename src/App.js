7b17512e635cd180620e30c4b3a0c2a9cedba8e7
diegonicita <diegonicita@gmail.com>
2022-05-09 16:51:23 -0300

    testing

--- src/App.js
+++ src/App.js
@@ -1,29 +1,58 @@
-import "./App.css"
+import "./App.css";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import Starship from "./Pages/Starship/Starship";
 import DungeonExplorer from "./Pages/DungeonExplorer/DungeonExplorer";
 import MyNavbar from "./Components/MyNavbar/MyNavbar";
-import Game from "./Components/Game/Game";
+import io from "socket.io-client";
 
-function App() { 
+const socket = io.connect("https://diego-test-server.herokuapp.com/");
 
-  const [gamePath, setGamePath] = React.useState("build_sistema_de_cultivos/Build/build_sistema_de_cultivos");
+function App() {
+  const joinRoom = () => {
+    socket.emit("join_room", 100);
+  };
+
+  const sendMessage = async () => {
+    const message = {
+      room: 100,
+      author: "Diego",
+      message: "Hola como te va ?",
+      time:
+        new Date(Date.now()).getHours() +
+        ":" +
+        new Date(Date.now()).getMinutes(),
+    };
+    await socket.emit("send_message", message);
+  };
+
+  React.useEffect(() => {
+    console.log("useEffect");
+    socket.on("receive_message", (data) => {
+      console.log("from useEffect: " + data.message);
+    });
+  }, [socket]);
 
   return (
-    <>        
-    <MyNavbar />
-    {/* <Game gamePath={gamePath}/> */}
+    <>
+      <MyNavbar />
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/starship" element={<Starship />}/>
-        <Route path="/dungeon_explorer" element={<DungeonExplorer />}/>
-      </Routes>    
+        <Route path="/" element={<Home />} />
+        <Route path="/starship" element={<Starship />} />
+        <Route path="/dungeon_explorer" element={<DungeonExplorer />} />
+      </Routes>
+      <div style={buttonStyle}>
+        <button onClick={joinRoom}> Join Room</button>
+        <button onClick={sendMessage}> Send Message </button>
+      </div>
     </>
-  
-    )
-
+  );
 }
+
+const buttonStyle = {
+  textAlign: "center",
+  margin: "0 auto",
+};
 
 export default App;
